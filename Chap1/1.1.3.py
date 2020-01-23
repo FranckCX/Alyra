793e09d35fa94e3df96ef4ae87fd7a6c92ff2d9d
FranckCX <chenfranckcmc@gmail.com>
2020-01-23 17:21:39 +0100

    1.1.3.py compliance

--- Chap1/1.1.3.py
+++ Chap1/1.1.3.py
@@ -1,8 +1,16 @@
-print("Ceci est un vérificateur de palindrome")
-var = input("Ecris un palindrome : ")
-var = str(var).lower()
+# print("Ceci est un vérificateur de palindrome")
+# var = input("Ecris un palindrome : ")
+# var = str(var).lower()
 
-if var == var[::-1]:
-    print(var,"est un palindrome")
-else:
-    print(var,"n'est pas un palindrome")
+# if var == var[::-1]:
+#     print(var,"est un palindrome")
+# else:
+#     print(var,"n'est pas un palindrome")
+
+def estPalindrome(str):
+    
+    palindrome = [c for c in str.lower() if c.isalpha()]
+    return (palindrome == palindrome[::-1])
+
+print(estPalindrome("ESOPE RESTE ICI ET SE REPOSE"))
+print(estPalindrome("ASSIETTE"))
