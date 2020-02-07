695c8dab0bfb2b52894928f9fc294451dd74ed0b
FranckCX <chenfranckcmc@gmail.com>
2020-02-07 16:38:22 +0100

    upd 3.1.5.sol

--- Chap3/3.1.5.sol
+++ Chap3/3.1.5.sol
@@ -18,9 +18,21 @@ contract Assemblee {
         admins.push(msg.sender);
     }
 
+    modifier verifMembre() {
+        require(estMembre(msg.sender),
+        "Il faut être membre.");
+        _;
+    }
+
+    modifier verifAdmin() {
+        require(estAdmin(msg.sender),
+        "Il faut être administrateur.");
+        _;
+    }
+
     function rejoindre() public {
         membres.push(msg.sender);
-        }
+    }
 
     function estMembre(address _utilisateur) public view returns (bool) {
         for (uint i = 0; i < membres.length; i++) {
@@ -42,16 +54,12 @@ contract Assemblee {
         }
     }
 
-    function proposition(string memory _proposition) public {
-        require(estMembre(msg.sender),
-        "Il faut être membre.");
+    function proposition(string memory _proposition) public verifMembre {
         Decision memory nouvelleProposition = Decision(_proposition,0,0);
         decisions.push(nouvelleProposition);
     }
 
-    function voter(uint _indice, bool _value) public {
-        require(estMembre(msg.sender),
-        "Il faut être membre.");
+    function voter(uint _indice, bool _value) public verifMembre {
         require(!decisions[_indice].aDejaVote[msg.sender],
         "Vous avez déjà voté !");
         if (_value == true) {
@@ -66,17 +74,11 @@ contract Assemblee {
         return int(decisions[_indice].pour - decisions[_indice].contre);
     }
 
-    function nommerAdmin(address _nouveauAdmin) private {
-        require(estMembre(_nouveauAdmin),
-        "Il faut être membre.");
-        require(estAdmin(msg.sender),
-        "Il faut être administrateur.");
+    function _nommerAdmin(address _nouveauAdmin) private verifMembre verifAdmin {
         admins.push(_nouveauAdmin);
     }
 
-    function demissionner() private {
-        require(estAdmin(msg.sender),
-        "Il faut être administrateur.");
+    function _demissionner() private verifAdmin {
         for (uint i = 0; i < admins.length; i++) {
             if (msg.sender == admins[i]) {
                 delete admins[i];
@@ -84,9 +86,7 @@ contract Assemblee {
         }
     }
 
-    function cloturerDecision(uint _numeroDecision) public {
-        require(estAdmin(msg.sender),
-        "Il faut être administrateur.");
+    function cloturerDecision(uint _numeroDecision) public verifAdmin {
         delete decisions[_numeroDecision];
     }
 
