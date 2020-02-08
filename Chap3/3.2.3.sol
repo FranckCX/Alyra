4eb0e64c2be0af8e03b4ac5b79f189f6216f835d
FranckCX <chenfranckcmc@gmail.com>
2020-02-08 20:53:43 +0100

    add 3.2.3.sol

---
+++ Chap3/3.2.3.sol
@@ -0,0 +1,63 @@
+pragma solidity ^0.5.10;
+
+import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+contract Cogere {
+
+    using SafeMath for uint256;
+
+    mapping (address => uint) organisateurs;
+
+    constructor() public {
+        organisateurs[msg.sender] = 100;
+    }
+
+    function transfererOrga(address orga, uint parts) internal {
+        require(estOrga(msg.sender), "Il faut être organisateur.");
+        organisateurs[orga] += parts;
+        organisateurs[msg.sender] -= parts;
+    }
+
+    function estOrga(address orga) public view returns (bool) {
+        if (organisateurs[orga] != 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
+
+contract CagnotteFestival is Cogere {
+
+    mapping (address => bool) festivaliers;
+    uint public placesRestantes;
+    uint private depensesTotales;
+    string[] public sponsors;
+
+    function acheterTicket() public payable {
+        require(msg.value >= 500 finney,
+        "Place à 0.5 Ethers");
+        require(placesRestantes > 0,
+        "Plus de places !");
+        festivaliers[msg.sender] = true;
+    }
+
+    function payer(address payable destinataire, uint montant) public {
+        require(estOrga(msg.sender));
+        require(destinataire != address(0));
+        require(montant > 0);
+        destinataire.transfer(montant);
+    }
+
+    function comptabiliserDepense(uint montant) private {
+        depensesTotales += montant;
+    }
+
+    function sponsoriser(string memory nom) public payable {
+        require(msg.sender != address(0));
+        require(msg.value >= 0.3 ether, "Don de 30 ethers minimum");
+        sponsors.push(nom);
+    }
+
+}
