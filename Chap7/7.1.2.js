e57dcf6072332eacb6dff4c8a9ac06a43ca0fef8
FranckCX <chenfranckcmc@gmail.com>
2020-02-28 15:31:55 +0100

    add 7.1.2.js

---
+++ Chap7/7.1.2.js
@@ -0,0 +1,39 @@
+// const readline = require("readline");
+// const fetch = require("node-fetch");
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// rl.question("Get Last Exchange rate for ", function(symbol) {
+//         fetch('https://api.bitfinex.com/v1/pubticker/' + symbol)
+//         .then(function(response) {
+//             return response.json();
+//         })
+//         .then(function(myJson) {
+//             console.log(`Last exchange rate (${symbol}) : ${myJson.last_price}$`);
+//         })
+//         .catch(err => console.error(err));
+//         console.log(`Last exchange rate (${symbol}) : ${myJson.last_price}$`);
+//         rl.close();
+// });
+
+// rl.on("close", function() {
+//     process.exit(0);
+// });
+
+ 
+const fetch = require("node-fetch");
+
+function getLastExchangeRateFor(symbol) {
+    fetch('https://api.bitfinex.com/v1/pubticker/' + symbol)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(myJson) {
+            console.log(`Last exchange rate for (${symbol}) : ${myJson.last_price}$`);
+        })
+        .catch(err => console.error(err));
+}
+
+getLastExchangeRateFor('btcusd')
